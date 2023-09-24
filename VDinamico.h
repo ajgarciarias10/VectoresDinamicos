49678d65e3cfc5e73539ffd20268135e5ea2438a
ajgarciarias10 <ajgarciarias@gmail.com>
2023-09-24 16:42:56 +0200

    Ejercicio 1a Hecho

--- VDinamico.h
+++ VDinamico.h
@@ -1,52 +1,60 @@
 //
 // Created by ajgar on 19/09/2023.
 //
+/**
+ * @file VDinamico.h
+ * @date 24/09/2023
+ * @author ajga0011 Abraham García Hurtado
+ * @title Vector Dinamico
+ */
 #include "stdexcept"
 #ifndef PR1_VDINAMICO_H
 #define PR1_VDINAMICO_H
 #include "algorithm"
 template<class T>
 class VDinamico {
+    private:
+        //Puntero a tipo generico
+        T *vector;
+        //Tamaño del vector sin signo long el entero mas grande
+        unsigned long int tama;
+    public :
+            //Constructor por defecto
+            VDinamico(unsigned  int n);
+            //Constructor de copia
+            VDinamico(const VDinamico<T>& origen);
+            //Constructor de copia por trozos
+            VDinamico(const VDinamico<T> &origen, unsigned  int posicioninicial, unsigned  int numElementos);
+            //Destructor
+            ~VDinamico();
+            //Operator igual
+            VDinamico<T> &operator = (const VDinamico <T> &origen);
+            //Operator corchetes se usa para acceder al vector
+            T &operator[](unsigned  long int pos);
+            //Ordenar de menor a mayor
+            void ordenar();
+            //Ordenar de mayor a menor
+            void ordenarMayorAmenor();
+    };
 
-private:
-    //Puntero a tipo generico
-    T *vector;
-    //Tamaño del vector sin signo long el entero mas grande
-    unsigned long int tama;
-public :
-        //Constructor por defecto
-        VDinamico(unsigned  int n);
-        //Constructor de copia
-        VDinamico(const VDinamico<T>& origen);
-        //Constructor de copia por trozos
-        VDinamico(const VDinamico<T> &origen, unsigned  int posicioninicial, unsigned  int numElementos);
-        //Destructor
-        ~VDinamico();
-        //Operator igual
-        VDinamico<T> &operator = (const VDinamico <T> &origen);
-        //Operator corchetes se usa para acceder al vector
-        T &operator[](unsigned  long int pos);
-        //Ordenar de menor a mayor
-        void ordenar();
-        //Ordenar de mayor a menor
-        void ordenarMayorAmenor();
-};
 
-template <class T>
-VDinamico<T>::~VDinamico() {
-    delete[] vector;
-}
-
-//Constructor por defecto
+/**
+ * @brief Constructor por defecto del vector dinamico
+ * @tparam T
+ * @param n Tamaño asignado al vector
+ */
 template<class T>
 VDinamico<T>::VDinamico(unsigned int n) {
     vector = new T[tama = n];
 }
-
-//Constructor copia
+/**
+ * @brief Constructor copia
+ * @tparam T
+ * @param origen Direccion de memoria del vector dinamico
+ *
+ */
 template<class T>
 VDinamico<T>::VDinamico(const VDinamico<T> &origen) {
-    //Constructor copia, comprobamos que no sea la misma direccion de memoria
         //Eliminamos el vector que teniamos
         delete[] vector;
         //Reservamos en memoria con el tamaño del que vamos a copiar
@@ -56,26 +64,46 @@ VDinamico<T>::VDinamico(const VDinamico<T> &origen) {
             vector[i] =origen.vector[i];
         }
 }
-
-//Constructor de copia parcial parametrizado
+/**
+ * @brief Constructor de copia parcial
+ * @tparam T
+ * @param origen Direccion de memoria del vector dinamico
+ * @param posicioninicial   Posicion inicial del vector dinamico
+ * @param numElementos      Le pasamos el numero de elementos maximo que vamos a utilizar en el main
+ */
 template<class T>
 VDinamico<T>::VDinamico(const VDinamico<T> &origen, unsigned  int posicioninicial, unsigned  int numElementos){
-
+    //Comprobamos si la posicion inicial supera el numero de elementos
     if(posicioninicial > numElementos){
         throw std::out_of_range("Ha susperado el limite de elementos");
     }
+    //Eliminamos el vector
     delete[]vector;
     //Creamos un vector con el tamaño asignado a la diferencia del numero de elementos con la posicion inical
     vector = new int[tama=numElementos-posicioninicial];
     for (int i = 0; i < tama; ++i) {
         vector[i] = origen.vector[posicioninicial+i];
     }
-
 }
+/**
+ * @brief Destructor del vector dinamico
+ * @tparam T
+ */
+template <class T>
+VDinamico<T>::~VDinamico() {
+    //Borramos el vector
+    delete[] vector;
+}
+/**
+ * @brief Operator igual pasamos la posicion memoria del vector de origen al vector dinamico
+ * @tparam T
+ * @param origen
+ * @return
+ */
 
-//Operador de asignación (=).
 template<class T>
 VDinamico<T> &VDinamico<T>::operator = (const VDinamico <T> &origen){
+    //Comprobamos que no sea la misma direccion de memoria
     if(this!=origen) {
         //Eliminamos el vector que teniamos
         delete[] vector;
@@ -88,9 +116,15 @@ VDinamico<T> &VDinamico<T>::operator = (const VDinamico <T> &origen){
     }
 }
 
-//Operador corchetes []
+/**
+ * @brief Operator corchete devuelve el contenido del  vector
+ * @tparam T
+ * @param pos Parametro en el que utilizamos la posicion del vector para poder obtener el contenido del vector
+ * @return
+ */
 template <class T>
 T &VDinamico<T>::operator[](unsigned  long int pos){
+    //Comprobamos si la posicion supera el tamaño
     if(pos >= tama){
         throw std::out_of_range("Ha superado el limite de elementos");
     }
@@ -98,13 +132,19 @@ T &VDinamico<T>::operator[](unsigned  long int pos){
     return vector[pos];
 }
 
-//Ordenar de menor a mayor
+/**
+ * @brief Ordena el vector de menor a mayor con la funcion sort
+ * @tparam T
+ */
 template <class T>
 void VDinamico<T>::ordenar() {
     std::sort(vector,vector + tama);
 }
 
-//Ordenar de mayor a menor
+/**
+ * @brief Ordena el vector de mayor a menor
+ * @tparam T
+ */
 template <class T>
 void VDinamico<T>::ordenarMayorAmenor(){
     std::sort(vector,vector + tama,std::greater<T>());
--- main.cpp
+++ main.cpp
@@ -3,40 +3,50 @@
 #include "stdlib.h"
 #include "time.h"
 #include "VDinamico.h"
-
+/**
+ * @brief Crear un vector dinamico de tamaño 1 millon y almacena valores aleatorios no repetidos
+ * Ademas de eso en funcion de lo que queramos ordena el vector
+ * @return
+ */
 int main() {
     int limite = 10000;
     //Definimos el vector dinamico con limite de elementos
     VDinamico<int> v(limite);
     int i =0;
-    //Flag para meter el valor dentro del vector
-    bool mete;
     //Inicializo un semilla aleatoria
     srand(time(NULL));
-    do{
-        //Obtenemos el numero aleatorio entre 1 y 1000000
-        int x = rand() %1000000 + 1;
-       //Si el numero esta en el vector
-        if(v[i] == x){
-            mete = true;
-        }
-        //Si no esta en el vector
-        if(!mete){
-            v[i] = x;
-        }
-        //Incrementa
-        i++;
-    } while (i < limite);
-    //Imprimo el vector menor a mayor
-    for (int j = 0; j <20 ; ++j) {
-        v.ordenar();
-        std::cout<<v[j]<<std::endl;
+    try{
+        do{
+            //Flag para meter el valor dentro del vector
+            bool mete = true;
+            //Obtenemos el numero aleatorio entre 1 y 1000000
+            int x = rand() %1000000 + 1;
+            //Si el numero esta en el vector
+            if(v[i] == x){
+                mete = false;
+            }
+            //Si no esta en el vector
+            if(mete){
+                v[i] = x;
+            }
+            //Incrementa
+            i++;
+        } while (i < limite);
+    }catch(std::bad_alloc &e){
+        std::cerr << "No hay suficiente espacio de memoria disponible" <<std::endl;
     }
+    //region Imprimo el vector menor a mayor
     /*
-    //Imprimo el vector mayor a menor
+        for (int j = 0; j <20 ; ++j) {
+            v.ordenar();
+            std::cout<<v[j]<<std::endl;
+        }
+    */
+    //endregion
+    //region Imprimo el vector mayor a menor
     for (int j = 0; j <i ; ++j) {
         v.ordenarMayorAmenor();
         std::cout<<v[j]<<std::endl;
     }
-*/
+    //endregion
 }
